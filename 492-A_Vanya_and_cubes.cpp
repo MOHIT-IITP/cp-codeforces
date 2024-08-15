a0f75507253c261b2f3b4e61d8a0c1448945d48d
MOHIT-IITP <codingbymohit@gmail.com>
2024-08-15 21:38:59 +0530

    update

--- 492-A_Vanya_and_cubes.cpp
+++ 492-A_Vanya_and_cubes.cpp
@@ -7,27 +7,27 @@ using namespace std;
 #define fast ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
 
 void solve(){
-  /*int n, cnt = 0,i=1,m=1;*/
-  /*cin>>n;*/
-  /*while(m<=n){*/
-  /*  m = ((i*i) + i)/2;*/
-  /*  if(m>n) break;*/
-  /*  n = n-m;*/
-  /*  cnt++;*/
-  /*  i++;*/
-  /*}*/
-  /*cout<<cnt<<endl;*/
-
-  int n;
+  int n, cnt = 0,i=1,m=1;
   cin>>n;
-  ll sum=0;
-  ll level=0;
-  while(sum<n){
-    sum+=level;
-    level++;
-    n-=sum;
+  while(m<=n){
+    m = ((i*i) + i)/2;
+    if(m>n) break;
+    n = n-m;
+    cnt++;
+    i++;
   }
-  cout<<level-1<<endl;
+  cout<<cnt<<endl;
+
+  /*int n;*/
+  /*cin>>n;*/
+  /*ll sum=0;*/
+  /*ll level=0;*/
+  /*while(sum<n){*/
+  /*  sum+=level;*/
+  /*  level++;*/
+  /*  n-=sum;*/
+  /*}*/
+  /*cout<<level-1<<endl;*/
 }
 
 int main(){
