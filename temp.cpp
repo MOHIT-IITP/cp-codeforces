cdb842d1fc6a684a46e04d1d21fe943c44b8f973
MOHIT-IITP <codingbymohit@gmail.com>
2024-08-15 21:17:17 +0530

    add template of cp

---
+++ temp.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define pb push_back;
+#define ll long long
+#define pb push_back
+#define MOD1 998244353
+#define MOD 1000000007 
+#define fast ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+void solve(){
+
+}
+
+int main(){
+  return 0;
+}
