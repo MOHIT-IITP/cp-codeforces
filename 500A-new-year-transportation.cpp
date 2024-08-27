6c7e801c350274eb93e8d4bcb495bd784d1064c3
MOHIT-IITP <codingbymohit@gmail.com>
2024-08-27 21:42:08 +0530

    done

--- 500A-new-year-transportation.cpp
+++ 500A-new-year-transportation.cpp
@@ -7,10 +7,18 @@ using namespace std;
 #define fast ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
 
 void solve(){
-
+  int n, t;
+  cin >> n >> t;
+  int c[n];
+  int p = 0;
+  for( int i = 0;i < n; i++) cin >> c[i];
+  while(p < t - 1) p += c[p];
+  if(p == t - 1) cout <<  " YES" << endl;
+  else cout << "NO" << endl;
 }
 
 int main(){
   fast;
+  solve();
   return 0;
 }
