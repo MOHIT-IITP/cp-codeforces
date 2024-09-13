6b8f2343c30a2dc592a812a6c179e6cbd37ed176
MOHIT-IITP <codingbymohit@gmail.com>
2024-09-13 20:03:15 +0530

    done daily question codeforces

---
+++ 959A.cpp
@@ -0,0 +1,27 @@
+// MOHIT-IITP
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define MOD1 998244353
+#define MOD 1000000007
+#define fast                                                                   \
+  ios_base::sync_with_stdio(0);                                                \
+  cin.tie(0);                                                                  \
+  cout.tie(0);
+
+void solve() {
+  int n;
+  cin >> n;
+  if (n % 2 == 0) {
+    cout << "Mahmoud";
+  } else {
+    cout << "Ehab";
+  }
+}
+
+int main() {
+  fast;
+  solve();
+  return 0;
+}
