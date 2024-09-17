206a68a9b2ac07045eca5fe45ac37863c132e718
MOHIT-IITP <codingbymohit@gmail.com>
2024-09-17 20:29:15 +0530

    done daily question codeforces

---
+++ 1294A-collecting-coins.cpp
@@ -0,0 +1,32 @@
+// MOHIT-IITP
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define MOD1 998244353
+#define MOD 1000000007
+#define fast                                                                   \
+  ios_base::sync_with_stdio(0);                                                \
+  cin.tie(0);                                                                  \
+  cout.tie(0);
+
+void solve() {
+  int a, b, c, n;
+  cin >> a >> b >> c >> n;
+  int total = a + b + c + n;
+  if (total % 3 != 0 || total / 3 < a || total / 3 < b || total / 3 < c) {
+    cout << "NO" << endl;
+  } else {
+    cout << "YES" << endl;
+  }
+}
+
+int main() {
+  fast;
+  int t;
+  cin >> t;
+  while (t--) {
+    solve();
+  }
+  return 0;
+}
