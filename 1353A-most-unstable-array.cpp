5378814bd13352cd5148efc6217158367df748b9
MOHIT-IITP <codingbymohit@gmail.com>
2024-09-19 10:20:27 +0530

    done question codeforces

---
+++ 1353A-most-unstable-array.cpp
@@ -0,0 +1,33 @@
+// MOHIT-IITP
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define MOD1 998244353
+#define MOD 1000000007
+#define fast                                                                   \
+  ios_base::sync_with_stdio(0);                                                \
+  cin.tie(0);                                                                  \
+  cout.tie(0);
+
+void solve() {
+  int n, k;
+  cin >> n >> k;
+  if (n == 1) {
+    cout << 0 << endl;
+  } else if (n == 2) {
+    cout << k << endl;
+  } else {
+    cout << k * 2 << endl;
+  }
+}
+
+int main() {
+  fast;
+  int t;
+  cin >> t;
+  while (t--) {
+    solve();
+  }
+  return 0;
+}
