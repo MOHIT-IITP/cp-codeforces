589aba3acd99787481e18b68b422ad58f7346ac5
MOHIT-IITP <codingbymohit@gmail.com>
2024-09-30 20:32:46 +0530

    done daily question of codeforces

--- 1195A-drinks-choosing.cpp
+++ 1195A-drinks-choosing.cpp
@@ -10,7 +10,29 @@ using namespace std;
   cin.tie(0);                                                                  \
   cout.tie(0);
 
-void solve() {}
+void solve() {
+    ll n , k;
+    cin>>n>>k;
+    ll x;
+
+    int cap =ceil(1.0*n/2);
+    int even = 0 , odd = 0;
+    vector<int> vec(k+1);
+    for(int i =0;i<n;i++){
+        cin>>x;
+        vec[x]++;
+    }
+    for(int i =0;i<=k;i++){
+        if(vec[i] % 2 ) even += vec[i]-1 , odd++; 
+        else even += vec[i];
+    }
+
+    int res=0;
+    res += 2*min(even / 2,cap);
+    cap -= min(even / 2, cap);
+    res+=cap;
+    cout<<res;
+}
 
 int main() {
   fast;
