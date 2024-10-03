e1d166c98f8208c3fb16ea62d601cac0cb871519
MOHIT-IITP <codingbymohit@gmail.com>
2024-10-03 21:43:26 +0530

    done daily code question on codeforces

---
+++ A. Competitive Programmer.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std
+// MOHIT-IITP template
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
--- A. Heating.cpp
+++ A. Heating.cpp
@@ -3,10 +3,21 @@ using namespace std;
 #define ll long long 
 
 void solve(){
-
+    ll c, sum;
+    cin>>c>>sum;
+    c = min(c, sum);
+    ll t = sum / c;
+    ll extra = sum % c;
+    ll total = c - extra;
+    ll ans = (t * t * total) + (extra * (t+1) * (t+1));
+    cout<<ans<<endl;
 }
 
 int main(){
-
+    int t;
+    cin>>t;
+    while(t--){
+        solve();
+    }
     return 0;
 }
