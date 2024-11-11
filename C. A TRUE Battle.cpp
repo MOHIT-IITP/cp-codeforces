429c04e6448f8b9487e6064b4eb163bd9de3858e
MOHIT-IITP <codingbymohit@gmail.com>
2024-11-11 22:18:14 +0530

    done

---
+++ C. A TRUE Battle.cpp
@@ -0,0 +1,22 @@
+// MOHIT-IITP template
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define MOD 1000000007
+
+int solve(){
+    int n, m;
+    cin>>n>>m;
+    // have to work on this
+
+}
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
