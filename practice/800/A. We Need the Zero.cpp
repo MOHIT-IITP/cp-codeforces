41669513ccf77660efe77f3e0ec1343aafa3296c
MOHIT-IITP <codingbymohit@gmail.com>
2025-10-06 12:31:45 +0530

    update something

---
+++ practice/800/A. We Need the Zero.cpp
@@ -0,0 +1,36 @@
+// MOHIT-IITP template
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define el endl
+#define pb push_back
+#define MOD 1000000007
+
+void solve() {
+  int n;cin>>n;
+  vector<int> a(n);
+  for(auto &x: a) cin>>x;
+
+  int temp = 0 ;
+  for(auto it : a){
+    temp = temp ^ it;
+  }
+  if(temp == 0){
+    cout<<"0"<<el;
+  }else{
+    if(n%2==1){
+      cout<<temp<<el;
+    }else{
+      cout<<-1<<el;
+    }
+  }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t; cin >> t;
+    while (t--) solve();
+    return 0;
+}
